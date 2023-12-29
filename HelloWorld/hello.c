c22e6b45e9c8bb0898cc53e7ab2bc8b3471ca432
ruby04-dev <ruby04.s.lee@gmail.com>
2023-12-29 15:33:47 +0900

    print Hello, World

---
+++ HelloWorld/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World\n");
+    return 0;
+
+    
+// shift+cmd+b 빌드
+// shift+cmd+r 런
+
+// cmd+k+c 주석 지정
+// cmd+k+u 주석 해제
+// 미래의 나에게 보내는 설명
+// 일기가 아니다.
+
+}
